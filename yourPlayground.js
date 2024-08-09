ca6700f90ac25f49f6531287308297e94d29931d
Sameer Baluch <balochtradersnayapay@gmail.com>
2024-08-09 17:34:39 +0500

    Update yourPlayground.js

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,4 +1,4 @@
-// ============>console.log<===================
+// ===============================================>console.log<===================
 // console.log kisi b data ko console me show krta he
 
 
@@ -8,6 +8,24 @@
 // console.log("and i am learning javascript");
 
 
+// ==============================<Data types in javascript>==========================================
+// javascript me mukhtalif data types hoti he jese k strings, number, booleans wgera wgera
+// for example:
+// Strings:== strings  ik data type he jo k just like simple text hota he lekin strings k under ager hum number ya koi or data type likhengey to wo b string bn jaega
+// string ko lihkney k liye double quote "" ya single  quote ''  use hota he is quote k under k data type ko string kehtey hen
+
+
+// number:========
+// number simple kisi b number ko kehtey hen for example 1,3,4,5,6,7,8,
+
+// converting string into number
+// kisi bhi string ko number me convert krney k 3 takreeekey hen
+
+// 1=======string k start me + likhney se wo number me convert hojata he for example
+// "4" ye ik string he lekin is k start me + lganey se ye number me convert hojaega +"4"
+
+// 2============string k start me number likhney se wo number me convert hojaega
+// "4" ye ik string he lekin is k start me number() lganey se ye number me convert hojaega number("4")
 
 
 
@@ -27,12 +45,12 @@
 
 
 
-// ==================================>Comment<========================================
+// =============================================>Comment<========================================
 // browser ko kisi bhi chez ko ignore krwaney k liye hum coment use krtey hen, for example agr humeny koi bhi code browser me show ya run nai krwana hota he to hum usey comment krtey he is k liye hum ye do bar slash // use krtey hen
 
 
 
-// ======================>operators<=================================
+// ===================================================>operators<=================================
 // operator use hotey hen kisi b value ko assign krney k liye, compare krney k liye or koi b arthmetc operation krney k liye just like plus+ minus- multiply * ya divide/ wgera wgera
 
 // var foodAmount = 10;
