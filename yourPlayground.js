c7a98eda63a4481e29bc93b24ca4bab80f0cd44b
Sameer Baluch <balochtradersnayapay@gmail.com>
2024-08-09 17:15:18 +0500

    Update yourPlayground.js

---
+++ yourPlayground.js
@@ -0,0 +1,64 @@
+// ============>console.log<===================
+// console.log kisi b data ko console me show krta he
+
+
+
+// console.log("hello world");
+// console.log("my name is sameer");
+// console.log("and i am learning javascript");
+
+
+
+
+
+
+// =======================>varraibales<==========================
+// varriables k under hum koi b value store krtey hen or usko koi name provide krtey hen for example
+// mera name sameer he to hum name k under sameer ko store krdengey like just like thhis below
+
+
+
+// var name = "sameer";
+// console.log(name);
+
+// var age = 21;
+// console.log(age);
+
+
+
+
+// ==================================>Comment<========================================
+// browser ko kisi bhi chez ko ignore krwaney k liye hum coment use krtey hen, for example agr humeny koi bhi code browser me show ya run nai krwana hota he to hum usey comment krtey he is k liye hum ye do bar slash // use krtey hen
+
+
+
+// ======================>operators<=================================
+// operator use hotey hen kisi b value ko assign krney k liye, compare krney k liye or koi b arthmetc operation krney k liye just like plus+ minus- multiply * ya divide/ wgera wgera
+
+// var foodAmount = 10;
+// var tipPercentage = 0.3;
+// var tipAmount = foodAmount * tipPercentage
+// console.log(tipAmount);
+
+
+
+// =======================================<prompt>===========================================
+// prompt user se koi b value ya data hasil krney k liye use krtey hen. prompt jb b use krtey hen to browser me ik popup show hita he jo user se koi b value or data mangta he
+
+
+// prompt("what is your name?")
+
+
+
+
+
+// ===================================<alert>============================================
+//  alert ik bhi prompt ki trah browser me popup show krta he lekin alert user se koi b data ya value nai mangta alert sirf user ko koi bhi information btaney k liye use hota he for example
+
+
+// alert("you are successfully loged in")
+
+
+
+
+
